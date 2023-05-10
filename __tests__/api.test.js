de412513e3074a8e9ac3a4760f7e5fe97e6a2e33
laumack <lauren.a.mcintyre@gmail.com>
2023-05-10 12:59:42 +0100

    add GET/api which responds with a JSON file with details of all the available endpoints

--- __tests__/api.test.js
+++ __tests__/api.test.js
@@ -3,6 +3,7 @@ const app = require("../routes/api.router.js");
 const connection = require("../db/connection.js");
 const testData = require("../db/data/test-data/index.js");
 const seed = require("../db/seeds/seed.js");
+const endpointsDocument = require("../endpoints.json");
 
 beforeEach(() => {
   return seed(testData);
@@ -10,6 +11,18 @@ beforeEach(() => {
 
 afterAll(() => {
   connection.end();
+});
+
+describe("/api - GET request", () => {
+  it("responds with a status code of 200 and a JSON describing all the available endpoints on the api", () => {
+    return request(app)
+      .get("/api")
+      .expect(200)
+      .expect("Content-Type", "application/json; charset=utf-8")
+      .then((response) => {
+        expect(JSON.parse(response.body.endpoints)).toEqual(endpointsDocument);
+      });
+  });
 });
 
 describe("/api/categories - GET request", () => {
@@ -31,8 +44,8 @@ describe("/api/categories - GET request", () => {
 describe("Error handling", () => {
   it("invalid path - responds with a status code of 404 and a specified error message if passed a route that does not exist", () => {
     return request(app)
-        .get("/api/invalidEndpoint")
-        .expect(404)
+      .get("/api/invalidEndpoint")
+      .expect(404)
       .then((response) => {
         expect(response.body.msg).toEqual("Invalid input");
       });
